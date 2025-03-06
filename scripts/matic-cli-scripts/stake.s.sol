d299f699fcfd40789d3384c0e6c74b4e42c84a1c
leovct <l009.vincent@gmail.com>
2025-03-06 16:02:41 +0100

    debug

--- scripts/matic-cli-scripts/stake.s.sol
+++ scripts/matic-cli-scripts/stake.s.sol
@@ -47,11 +47,7 @@ contract MaticStake is Script {
     
     console.log("Validator set size : ", stakeManager.currentValidatorSetSize());
 
-
-    stakeManager.stakeForPOL(validatorAccount, stakeAmount, heimdallFee, true, pubkey);
-
-
-
+    // stakeManager.stakeForPOL(validatorAccount, stakeAmount, heimdallFee, true, pubkey);
   }
 
   function topUpForFee() public {
